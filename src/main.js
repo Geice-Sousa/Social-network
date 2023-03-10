b00e2266f4188bbe716af70018458c6110942580
Geice Sousa Pinho <geicesousa@outlook.com>
2023-03-10 17:12:34 -0300

    pagina de cadastro css e js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
+import cadastro from './pages/cadastro/cadastro.js';
 import login from './pages/login/login.js';
 
 const main = document.querySelector('#main');
 
 const exibirLogin = () => {
-  // const paginaLogin = login();
-
   main.appendChild(login());
 };
 
 exibirLogin();
+
+const exibirCadastro = () => {
+  main.appendChild(cadastro());
+};
+
+exibirCadastro();
